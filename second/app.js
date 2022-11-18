d7019292ed39a8cb1db70888545af218f6d837d9
zabierowski <zabierowski@nomachine.com>
2022-11-18 17:01:19 +0100

    express routing

--- second/app.js
+++ second/app.js
@@ -1,23 +1,51 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+
+const jsonFile = require('./file.json');
 
 const app = express();
 const port = 5500;
 
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, './public')));
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, "./index.html"));
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
-app.get("/about", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "./about.html"));
+app.get('/about', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, './views/about.html'));
 });
 
-app.all("*", (req, res) => {
-  res.status(404).send("Page not found");
+app.get('/request', (req, res) => {
+    res.status(200).json({ id: 5, name: 'Ok', age: 'Old' });
+});
+
+app.get('/serve', (req, res) => {
+    res.status(200).json(jsonFile);
+});
+
+app.get('/serve/:index', (req, res) => {
+    if (!Number.isInteger(Number(req.params.index))) {
+        res.status(405).send('invalid index');
+        return;
+    }
+
+    const jsonResponse = jsonFile.find(
+        (value) => value.index === Number(req.params.index)
+    );
+
+    if (!jsonResponse) {
+        res.status(404).send('Index out of range');
+        return;
+    }
+
+    res.status(200).json(jsonResponse);
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('Page not found');
 });
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+    console.log(`Server started on port ${port}`);
 });
---
+++ second/data.js
@@ -0,0 +1,7 @@
+const user = {
+  id: 10,
+  name: "aaaaa",
+  age: "5",
+};
+
+module.exports = user;
